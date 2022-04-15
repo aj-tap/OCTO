6f4ec9c3295af5302bd022bab67f2926385a766e
axojolotl <wafforyu@gmail.com>
2022-04-16 00:51:22 +0800

    minor changes in container

--- gui/container.py
+++ gui/container.py
@@ -1,10 +1,20 @@
 from tkinter import Frame
 
+"""
+Here we create our own Frame object called
+Container in order for to set our own 
+configurations and write more modular code.
+
+The superclass constructor can accept the
+parameters of the superclass (Frame) so we 
+can pass in highlight-background and thickness.
+"""
+
 
 class Container(Frame):
     def __init__(self):
-        # this is the constructor for Frame, so we can add Frame parameters
         super().__init__(highlightbackground='gray', highlightthickness=1)
+
         self.pack(side="top", fill="both", expand=True, pady=5, padx=5)
 
         self.grid_rowconfigure(0, weight=1)
