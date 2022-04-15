6e762004312db55d5b64b1e309ae5dc4f69536d1
axojolotl <wafforyu@gmail.com>
2022-04-15 23:37:24 +0800

    minor tweaks

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from src.gui_menu import GuiMenu
 from src.traffic_bot import TrafficBot
 
-sample = TrafficBot("..\yolo-coco-V4", "..\input.mp4", "..\output")
-
-sample.vidFrameChecker()  ## will return vidframe void.
-sample.runBot()
-
 # Driver code
 if __name__ == "__main__":
+    # sample
     app = GuiMenu()
     app.mainloop()
 
+    # sample
+    sample = TrafficBot("..\yolo-coco-V4", "..\input.mp4", "..\output")
+
+    sample.vidFrameChecker()  ## will return vidframe void.
+    sample.runBot()
