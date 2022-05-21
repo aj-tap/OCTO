5b6227fd6c363120e46c472b64e3d9993f84e441
axojolotl <wafforyu@gmail.com>
2022-05-21 10:59:56 +0800

    remove old comments

--- main.py
+++ main.py
@@ -2,18 +2,5 @@ from threading import Thread
 from gui.app import App
 
 if __name__ == "__main__":
-    # bot = TrafficBot("yolo-coco-V4", "sample.mp4", "output")
-
-    # TEMPORARY; to be moved to app
     app = App()
     app.mainloop()
-    # Thread(target=app2.mainloop()).start()
-
-    # sample.run_bot()
-
-"""
-TO UPDATE:
-* fix output directory bug, frames don't show up in the output dir
-* fix window not responding bug on runbot (tried threading, didnt work),
-try async await?
-"""
